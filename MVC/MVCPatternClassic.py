6f154af1df2de32d69b5f9e20d308b734c9435c1
Hemant Jain <hjain1011@gmail.com>
2019-02-12 00:44:55 +0530

    Update

--- MVC/MVCPatternClassic.py
+++ MVC/MVCPatternClassic.py
@@ -11,13 +11,20 @@ class Model(object):
         return self.data
 
 class View(object):
-    def update(self, data):
-        print("View: Updating the view with data: ", data)
+    # Dummy view which is print some data to standard output.
+    # View is supposed to intaract the UI. 
+    def __init__(self, model):
+        self.model = model
+
+    # In classic MVC view interact with the model to get data.
+    def update(self):
+        data = self.model.getData()
+        print("View: Updating the view with data : ", data)
 
 class Controller(object):
     def __init__(self):
         self.model = Model()
-        self.view = View()
+        self.view = View(self.model)
 
     def setData(self, data):
         print("Controller: Receive data from client.")
@@ -25,11 +32,9 @@ class Controller(object):
 
     def updateView(self):
         print("Controller: Receive update view from client.")
-        data = self.model.getData()
-        self.view.update(data)
+        self.view.update()
 
 class Client(object):
-    print("Client: Pass trigger to Controller.")
     controller = Controller()
     controller.updateView()
 
---
+++ MVC/MVCPatternClassicStudent.py
@@ -0,0 +1,50 @@
+class Student:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
+class Model(object):
+    # Dummy model which just have one object.  
+    # Model is supposed to interact with database.
+    def __init__(self):
+        self.st = Student("Harry", 1)
+
+    def setData(self, name, id):
+        print("Model: Set data :", name, id)
+        self.st.name = name
+        self.st.id = id
+
+    def getData(self):
+        print("Model: Get data.")
+        return self.st
+
+class View(object):
+    # Dummy view which is print some data to standard output.
+    # View is supposed to intaract the UI. 
+    def __init__(self, model):
+        self.model = model
+
+    # In classic MVC view interact with the model to get data.
+    def update(self):
+        st = self.model.getData()
+        print("View: Student Info : ", st.name , st.id)
+
+class Controller(object):
+    def __init__(self):
+        self.model = Model()
+        self.view = View(self.model)
+
+    def setData(self, name, id):
+        print("Controller: Receive data from client.")
+        self.model.setData(name, id)
+
+    def updateView(self):
+        print("Controller: Receive update view from client.")
+        self.view.update()
+
+class Client(object):
+    controller = Controller()
+    controller.updateView()
+
+    controller.setData("jack", 2)
+    controller.updateView()
